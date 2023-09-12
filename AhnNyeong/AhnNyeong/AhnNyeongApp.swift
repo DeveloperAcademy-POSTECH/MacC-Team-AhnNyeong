c6ac511e2d68af75fb967155639008963c837bff
jaelyung <jaekim7724@gmail.com>
2023-09-12 15:33:29 +0900

    [chore] 프로젝트 파일 최초생성

---
+++ AhnNyeong/AhnNyeong/AhnNyeongApp.swift
@@ -0,0 +1,17 @@
+//
+//  AhnNyeongApp.swift
+//  AhnNyeong
+//
+//  Created by jaelyung kim on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct AhnNyeongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
