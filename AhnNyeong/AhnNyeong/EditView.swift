240e39695cdfe4f9b5785e097abb5cbdeb190890
jaelyung <jaekim7724@gmail.com>
2023-10-18 22:23:57 +0900

    [feat] 생리주기 데이터 수정 View 최초생성 - 저장된 데이터 값을 불러오고 해당 값을 변경한 뒤 확인버튼을 누르면 데이터 수정 됨

--- AhnNyeong/AhnNyeong/EditView.swift
+++ AhnNyeong/AhnNyeong/EditView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct EditView: View {
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    @ObservedObject var mensInfoStore: MensInfoStore
+    @Binding var selectedMensInfo: MensInfo
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: HorizontalAlignment.leading) {
+                Text("imperID")
+                    .font(.headline)
+                TextField("Enter imperID", text: $selectedMensInfo.imperID)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            .padding()
+            
+            Button  {
+                let editMensInfo = MensInfo(
+                    id: selectedMensInfo.id,
+                    imperID: selectedMensInfo.imperID,
+                    mensAmt: selectedMensInfo.mensAmt,
+                    mensSymp: selectedMensInfo.mensSymp,
+                    emoLv: selectedMensInfo.emoLv,
+                    regDe: selectedMensInfo.regDe
+                )
+                mensInfoStore.editMensInfo(mensInfo: editMensInfo)
+                self.mode.wrappedValue.dismiss()
+            } label: {
+                Text("확인")
+            }
+        }
     }
 }
 
-#Preview {
-    EditView()
-}
+//#Preview {
+//    EditView()
+//}
