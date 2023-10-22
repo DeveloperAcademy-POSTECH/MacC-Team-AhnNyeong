2b81402bb448fcc2ee9b094dcf663d6da14f87e2
jaelyung <jaekim7724@gmail.com>
2023-10-22 20:33:48 +0900

    [feat] dateFormatter 추가, 레이아웃 VStack 제거

--- AhnNyeong/AhnNyeong/MVP/ImperView.swift
+++ AhnNyeong/AhnNyeong/MVP/ImperView.swift
@@ -16,7 +16,13 @@ struct ImperView: View {
     let mensSymp = ["없음", "있음", "아주심함"]
     let mensAmt = ["적음", "보통", "많음"]
     let emoLv = ["보통", "좋음", "나쁨"]
+    let dateformat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY년 M월 d일"
+        return formatter
+    }()
     var body: some View {
+        
         VStack {
             Button(action: {
                 isHaveMens.toggle()
@@ -26,62 +32,86 @@ struct ImperView: View {
                     .frame(width: 120, height: 120)
             })
             if isHaveMens {
-                VStack {
-                    Text("생리통")
-                        .font(.largeTitle)
-                    HStack {
-                        ForEach(0..<3) { index in
-                            Button(action: {
-                                self.mensSympSelected = index
-                            }, label: {
-                                Image(systemName: "drop.circle.fill")
-                                    .resizable()
-                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                                    .foregroundColor(self.mensSympSelected == index ? Color.blue : Color.green)
-                            })
-                        }
-                    }
-                    Text("생리양")
-                        .font(.largeTitle)
-                    HStack {
-                        ForEach(0..<3) { index in
-                            Button(action: {
-                                self.mensAmtSelected = index
-                            }, label: {
-                                Image(systemName: "drop.circle.fill")
-                                    .resizable()
-                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                                    .foregroundColor(self.mensAmtSelected == index ? Color.blue : Color.green)
-                            })
-                        }
-                    }
-                    Text("감정상태")
-                        .font(.largeTitle)
-                    HStack {
-                        ForEach(0..<3) { index in
-                            Button(action: {
-                                self.emoLvSelected = index
-                            }, label: {
-                                Image(systemName: "drop.circle.fill")
-                                    .resizable()
-                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                                    .foregroundColor(self.emoLvSelected == index ? Color.blue : Color.green)
-                            })
-                        }
-                    }
-                    Button(action: {
-                        let formatter = ISO8601DateFormatter()
-                        mensInfoStore.addNewMensInfo(
-                            mensInfo: MensInfo(id: UUID().uuidString, imperID: "abc123"
-                                               , mensAmt: mensAmt[mensAmtSelected], mensSymp: mensSymp[mensSympSelected], emoLv: emoLv[emoLvSelected], regDe: formatter.string(from: Date())))
-                    }, label: {
-                        Text("저장하기")
-                    })
+                HStack {
+                    Text(dateformat.string(from: Date()))
+                        .padding()
+                        .font(.title2)
+                    Spacer()
                 }
+                Text("생리통")
+                    .font(.largeTitle)
+                HStack {
+                    ForEach(0..<3) { index in
+                        Button(action: {
+                            self.mensSympSelected = index
+                        }, label: {
+                            if self.mensSympSelected == index {
+                                Image("mensSymp\(index+1)")
+                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                            } else {
+                                Image("mensSymp\(index+1)_un")
+                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                            }
+                            
+                        })
+                    }
+                }
+                Text("생리양")
+                    .font(.largeTitle)
+                HStack {
+                    ForEach(0..<3) { index in
+                        Button(action: {
+                            self.mensAmtSelected = index
+                        }, label: {
+                            if self.mensAmtSelected == index {
+                                Image("mensAmt\(index+1)")
+                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                            } else {
+                                Image("mensAmt\(index+1)_un")
+                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                            }
+                        })
+                    }
+                }
+                Text("감정상태")
+                    .font(.largeTitle)
+                HStack {
+                    ForEach(0..<3) { index in
+                        Button(action: {
+                            self.emoLvSelected = index
+                        }, label: {
+                            if self.emoLvSelected == index {
+                                Image("emoLv\(index+1)")
+                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                            } else {
+                                Image("emoLv\(index+1)_un")
+                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                            }
+                        })
+                    }
+                }
+                Button(action: {
+                    mensInfoStore.addNewMensInfo(
+                        mensInfo: MensInfo(id: UUID().uuidString, imperID: "abc123"
+                                           , mensAmt: mensAmt[mensAmtSelected], mensSymp: mensSymp[mensSympSelected], emoLv: emoLv[emoLvSelected], regDe: dateformat.string(from: Date())))
+                }, label: {
+                    Rectangle()
+                        .cornerRadius(15)
+                        .foregroundColor(.secondary)
+                        .frame(height: 60)
+                        .overlay {
+                            Text("저장하기")
+                                .font(.headline)
+                                .foregroundColor(.black)
+                        }
+                })
+                .padding()
             }
         }
     }
 }
+
+
 
 #Preview {
     ImperView()
