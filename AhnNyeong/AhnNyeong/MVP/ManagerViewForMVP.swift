95e074b377cc3835df97fca081aa47308c78a588
zyeongPark <piiy10141997@gmail.com>
2023-10-23 15:48:06 +0900

    [Feat] 관리자의 대상자 생리정보 조회 뷰를 list 형태로 전환하였습니다.

--- AhnNyeong/AhnNyeong/MVP/ManagerViewForMVP.swift
+++ AhnNyeong/AhnNyeong/MVP/ManagerViewForMVP.swift
@@ -4,36 +4,21 @@
 //
 //  Created by qwd on 10/22/23.
 //
-
+//
 import SwiftUI
 
 struct ManagerViewForMVP: View {
     @Binding var selectedUserType: ContentView.LoginType
-    let currentDate = Date()
     @StateObject var mensInfoStore: MensInfoStore = MensInfoStore()
     @State var isClicked = false
     var body: some View {
-        VStack {
-            HStack(spacing: 100){
-                Text("김머쪄 님의 생리 정보")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                NavigationLink(destination: LoginTypeView(selectedUserType: $selectedUserType), label: {
-                    Button(action: {
-                        selectedUserType = .notyet
-                    }, label: {
-                        Image(systemName: "arrow.uturn.backward")
-                            .resizable()
-                            .frame(width:30, height: 30)
-                            .foregroundColor(.pink)
-                    })
-                })
-            }
-            ScrollView(showsIndicators: false) {
+        NavigationStack {
+            List {
                 ForEach(mensInfoStore.mensInfos, id: \.self) { mensInfo in
                     ManagerListView(mensInfo: mensInfo, mensInfoStore: mensInfoStore)
                 }
             }
+
             .onAppear {
                 mensInfoStore.listenToRealtimeDatabase()
             }
@@ -41,7 +26,15 @@ struct ManagerViewForMVP: View {
                 mensInfoStore.stopListening()
             }
         }
-        .padding(.top, 10)
+        .navigationBarTitle("김생리님의 생리 정보")
+        .navigationBarItems(leading:
+            Button(action: {
+                // 뒤로 돌아가게
+                selectedUserType = .notyet
+            }) {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.pink)
+            })
     }
 }
 
@@ -49,44 +42,27 @@ struct ManagerListView: View {
     @State var mensInfo: MensInfo
     var mensInfoStore: MensInfoStore
     var body: some View {
-        VStack(alignment: .leading, spacing: 5) {
-            Text(mensInfo.regDe)
-                .font(.title3)
-                .fontWeight(.semibold)
-            Rectangle()
-                .frame(width: 362, height: 120)
-                .foregroundColor(.teal)
-                .cornerRadius(20)
-                .overlay {
-                    HStack {
-                        Circle()
-                            .foregroundColor(.white)
-                            .frame(width: 100, height: 100)
-                            .overlay(
-                                Text("생리통 \n\(mensInfo.mensSymp)")
-                                    .font(.headline)
-                                    .multilineTextAlignment(.center)
-                            )
-                        Circle()
-                            .foregroundColor(.white)
-                            .frame(width: 100, height: 100)
-                            .overlay(Text("생리양 \n\(mensInfo.mensAmt)")
-                                .font(.headline)
-                                .multilineTextAlignment(.center)
-                            )
-                        Circle()
-                            .foregroundColor(.white)
-                            .frame(width: 100, height: 100)
-                            .overlay(Text("감정 \n\(mensInfo.emoLv)")
-                                .font(.headline)
-                                .multilineTextAlignment(.center)
-                            )
-                    }
-                }
-        }.padding()
-
+        Section(mensInfo.regDe){
+            HStack {
+                Text("생리통")
+                Spacer()
+                Text(mensInfo.mensSymp)
+            }
+            HStack {
+                Text("생리 양")
+                Spacer()
+                Text(mensInfo.mensAmt)
+            }
+            HStack {
+                Text("감정 상태")
+                Spacer()
+                Text(mensInfo.emoLv)
+            }
+        }
     }
 }
+
 //#Preview {
 //    ManagerViewForMVP()
 //}
+
