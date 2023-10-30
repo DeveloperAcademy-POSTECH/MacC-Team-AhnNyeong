98c0d6c12548cc0608d9006c066ade425fd314e5
zyeongPark <piiy10141997@gmail.com>
2023-10-30 13:36:19 +0900

    [feat][#25] 관리자의 대상자 입력뷰 & 대상자의 메인 뷰 레이아웃 완성

---
+++ AhnNyeong/AhnNyeong/ManagerTargetInputView.swift
@@ -0,0 +1,18 @@
+//
+//  ManagerTargetInputView.swift
+//  AhnNyeong
+//
+//  Created by qwd on 10/27/23.
+//
+
+import SwiftUI
+
+struct ManagerTargetInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManagerTargetInputView()
+}
---
+++ AhnNyeong/AhnNyeong/TargetMainView.swift
@@ -0,0 +1,18 @@
+//
+//  TargetMainView.swift
+//  AhnNyeong
+//
+//  Created by qwd on 10/30/23.
+//
+
+import SwiftUI
+
+struct TargetMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TargetMainView()
+}
