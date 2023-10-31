64f1f84d514b08e216b96cbcb6870757e738adce
jaelyung <jaekim7724@gmail.com>
2023-11-01 00:38:09 +0900

    [feat] NavigationLink를 제거하고 .navigationDestination 적용 - 증상입력창으로 넘어가는 버튼과 달력보기 버튼에 Button 코드 적용 - 물방울 Button 클릭시 파티클 적용을 위한 딜레이 코드 추가

--- AhnNyeong/AhnNyeong/TargetMainView.swift
+++ AhnNyeong/AhnNyeong/TargetMainView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct TargetMainView: View {
     @State var startDefaultBtnClick = false
+    @State var calendarBtnClick = false
+    @State var toSympView = false
     var body: some View {
         NavigationStack {
             VStack(spacing: 0) {
@@ -23,25 +25,36 @@ struct TargetMainView: View {
                     }
                 }
                 .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
-                NavigationLink(destination: SympView()) {
-                    //MARK: 에러날 시 NavigationLink 전체 주석하고 사용
-                    Image("startDefault")
-                    .padding(.top, 63)
-                }
+                Button(action: {
+                    startDefaultBtnClick = true
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        toSympView = true
+                    }
+                }, label: {
+                    Image(startDefaultBtnClick ? "startPressed" : "startDefault")
+                        .padding(.top, 63)
+                })
                 Text("생리를 시작했나요?")
                     .font(.system(size: 30, weight: .bold))
                     .padding(.top, 40)
-                NavigationLink(destination: CalView()) { //MARK: CalView() 파일명 교체필요, 에러날 시 NavigationLink 전체 주석하고 사용
+                Button(action: {
+                    calendarBtnClick = true
+                }, label: {
                     Text("\(Image(systemName: "calendar")) 달력 보기")
                         .font(.system(size: 22, weight: .bold))
                         .foregroundColor(Color(.coral400))
                         .padding(.init(top: 10, leading: 15, bottom: 10, trailing: 15))
                         .background(RoundedRectangle(cornerRadius: 10).fill(.white).shadow(color: Color(.sRGBLinear, white: 0, opacity: 0.15), radius: 8, x: 0, y: 4))
-                    //MARK: - Button Extension 적용필요
-                }
+                })// MARK: - Button Extension 적용필요
                 .padding(.top, 40)
             }
             .padding(EdgeInsets(top: 30, leading: 16, bottom: 30, trailing: 16))
+            .navigationDestination(isPresented: $toSympView) {
+                SympView()
+            }
+            .navigationDestination(isPresented: $calendarBtnClick) {
+                CalView()
+            }
         }
     }
 }
