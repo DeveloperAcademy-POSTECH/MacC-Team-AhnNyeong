f354ab5768cebfad8d43f9618140fcd635df570e
zyeongPark <piiy10141997@gmail.com>
2023-11-01 19:25:41 +0900

    [Style][#35] 뷰 네이밍 및 폴더링 완료하였고, 에셋 추가 및 폴더링, 컬러에 MARK로 주석처리 및 가독성 높이는 코드로 수정 완료했습니다.

---
+++ AhnNyeong/AhnNyeong/UI/TargetViews/SympView.swift
@@ -0,0 +1,18 @@
+//
+//  SympView.swift
+//  AhnNyeong
+//
+//  Created by qwd on 11/1/23.
+//
+
+import SwiftUI
+
+struct SympView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SympView()
+}
