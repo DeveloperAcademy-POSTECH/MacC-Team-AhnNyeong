712058d77aef06fb6e3317fa356b9a14b46c4f5f
jaelyung <jaekim7724@gmail.com>
2023-11-01 11:49:55 +0900

    [feat] Color Extension 파일 최초생성

---
+++ AhnNyeong/AhnNyeong/Color.swift
@@ -0,0 +1,35 @@
+//
+//  Color.swift
+//  AhnNyeong
+//
+//  Created by jaelyung kim on 11/1/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let coral50 = Color(.coral50)
+    static let coral100 = Color(.coral100)
+    static let coral200 = Color(.coral200)
+    static let coral300 = Color(.coral300)
+    static let coral400 = Color(.coral400)
+    static let coral500 = Color(.coral500)
+    static let coral600 = Color(.coral600)
+    static let coral700 = Color(.coral700)
+    static let coral800 = Color(.coral800)
+    static let coral900 = Color(.coral900)
+    static let white50 = Color(.white50)
+    static let white75 = Color(.white75)
+    static let white100 = Color(.white100)
+    static let white200 = Color(.white200)
+    static let white300 = Color(.white300)
+    static let white400 = Color(.white400)
+    static let white500 = Color(.white500)
+    static let black50 = Color(.black50)
+    static let black75 = Color(.black75)
+    static let black100 = Color(.black100)
+    static let black200 = Color(.black200)
+    static let black300 = Color(.black300)
+    static let black400 = Color(.black400)
+    static let black500 = Color(.black500)
+}
