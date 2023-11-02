3953c6d2dd063c6dacbb19833591cbd669b418d4
oceanooooo <funboard6287@gmail.com>
2023-11-02 18:43:31 +0900

    [feat] DetailTermView 구현

---
+++ AhnNyeong/AhnNyeong/UI/SettingViews/DetailTermView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailTermView.swift
+//  AhnNyeong
+//
+//  Created by OhSuhyun on 2023.11.02.
+//
+
+import SwiftUI
+
+struct DetailTermView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailTermView()
+}
