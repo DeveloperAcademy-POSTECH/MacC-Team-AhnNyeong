d28b219832b0ca60533d72ef45fb5c79c63c15be
jaelyung <jaekim7724@gmail.com>
2023-11-04 23:14:06 +0900

    [feat] NextMensInfoView 분리 생성

---
+++ AhnNyeong/AhnNyeong/UI/AdminViews/NextMensInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  NextMensInfoView.swift
+//  AhnNyeong
+//
+//  Created by jaelyung kim on 11/4/23.
+//
+
+import SwiftUI
+
+struct NextMensInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NextMensInfoView()
+}
