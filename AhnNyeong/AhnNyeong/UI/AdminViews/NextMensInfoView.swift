0f2073ee2ab21316e37c385e0d9098679dd0f456
jaelyung <jaekim7724@gmail.com>
2023-11-05 00:01:41 +0900

    [feat] NextMensInfoView 분리 생성 - 곧 생리 예정 배경 View - LazyVGrid 컴포넌트 - 내부 태그 텍스트 배경

--- AhnNyeong/AhnNyeong/UI/AdminViews/NextMensInfoView.swift
+++ AhnNyeong/AhnNyeong/UI/AdminViews/NextMensInfoView.swift
@@ -8,11 +8,124 @@
 import SwiftUI
 
 struct NextMensInfoView: View {
+    var title: String
+    let column = [GridItem(.flexible(), alignment: .top), GridItem(.flexible(), alignment: .top)]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(title)
+                    .bold24Black300()
+                HStack(spacing: 10) {
+                    LazyVGrid(columns: column, spacing: 10, content: {
+                        HStack {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text("김이최박생리")
+                                    .medium16Black500()
+                                HStack(spacing: 5) {
+                                    Image("Drop")
+                                    Text("D-1")
+                                        .medium16Black500()
+                                }
+                                Group {
+                                    Text("과다월경")
+                                    Text("도전적 행동 주의")
+                                }
+                                .modifier(Regular12Black500())
+                                .padding(EdgeInsets(top: 2, leading: 4, bottom: 2, trailing: 4))
+                                .background(TagBackgroundView())
+                            }
+                            Spacer()
+                        }
+                        .padding(10)
+                        .background(NextMensInfoDetailBox())
+                        HStack {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text("김이최박생리")
+                                    .medium16Black500()
+                                HStack(spacing: 5) {
+                                    Image("Drop")
+                                    Text("D-3")
+                                        .medium16Black500()
+                                }
+                                Group {
+                                    Text("불규칙한 주기")
+                                }
+                                .modifier(Regular12Black500())
+                                .padding(EdgeInsets(top: 2, leading: 4, bottom: 2, trailing: 4))
+                                .background(TagBackgroundView())
+                            }
+                            Spacer()
+                        }
+                        .padding(10)
+                        .background(NextMensInfoDetailBox())
+                        HStack {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text("김이최박생리")
+                                    .medium16Black500()
+                                HStack(spacing: 5) {
+                                    Image("Drop")
+                                    Text("D-5")
+                                        .medium16Black500()
+                                }
+                            }
+                            Spacer()
+                        }
+                        .padding(10)
+                        .background(NextMensInfoDetailBox())
+                        HStack {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text("김이최박생리")
+                                    .medium16Black500()
+                                HStack(spacing: 5) {
+                                    Image("Drop")
+                                    Text("D-7")
+                                        .medium16Black500()
+                                }
+                                Group {
+                                    Text("과다월경")
+                                }
+                                .modifier(Regular12Black500())
+                                .padding(EdgeInsets(top: 2, leading: 4, bottom: 2, trailing: 4))
+                                .background(TagBackgroundView())
+                            }
+                            Spacer()
+                        }
+                        .padding(10)
+                        .background(NextMensInfoDetailBox())
+                    })
+                }
+            }
+            Spacer()
+        }
+        .padding(EdgeInsets(top: 20, leading: 16, bottom: 20, trailing: 16))
+        .background(NextMensInfoBackground())
+    }
+}
+
+struct NextMensInfoBackground: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(.white50)
+            .shadow(color: .black500.opacity(0.15), radius: 4, x: 0, y: 0)
+    }
+}
+
+struct NextMensInfoDetailBox: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .inset(by: 0.25)
+            .stroke(Color.black75, lineWidth: 0.5)
+            .foregroundColor(Color.white50)
+    }
+}
+
+struct TagBackgroundView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 5)
+            .foregroundColor(Color.coral100)
     }
 }
 
 #Preview {
-    NextMensInfoView()
+    NextMensInfoView(title: "곧 생리 예정")
 }
