6dcce8324c11fb4dd88962337c26450424c1dcf6
zyeongPark <piiy10141997@gmail.com>
2023-11-05 06:58:31 +0900

    [fix][#50] github stash 해결 테스트용 커밋

--- AhnNyeong/AhnNyeong/UI/AdminViews/AdminMngView.swift
+++ AhnNyeong/AhnNyeong/UI/AdminViews/AdminMngView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct AdminMngView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("test")
+            }.navigationTitle("이용자 리스트")
+        }
     }
 }
 
