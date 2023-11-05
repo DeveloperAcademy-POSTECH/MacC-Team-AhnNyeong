a473f579bd31e2ff970692651b368ac7d79224da
zyeongPark <piiy10141997@gmail.com>
2023-11-05 12:47:51 +0900

    [chore][#50] textmodifier와 gesturemodifier를 모두 resources 하위에 위치하도록 폴더링 하였습니다.

--- AhnNyeong/AhnNyeong/UI/AdminViews/AdminMngView.swift
+++ AhnNyeong/AhnNyeong/UI/AdminViews/AdminMngView.swift
@@ -10,15 +10,27 @@ import SwiftUI
 struct AdminMngView: View {
     var body: some View {
         ZStack {
-            Color.white300.ignoresSafeArea()
+            Color.black.ignoresSafeArea(.all)
             NavigationStack {
                 VStack {
-                    Text("개별 상세 정보를 열람, 수정할 수 있습니다.")
-                        .medium14Black500()
+                    MngViewTitle()
+                    VStack{
+                        
+                    }
                 }
-                .navigationTitle("이용자 리스트")
+//                .navigationTitle("이용자 리스트")
             }
-            .background(Color.white300)
+        }
+    }
+}
+struct MngViewTitle: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text("이용자 리스트")
+                .bold28Black500()
+                .foregroundColor(.black400)
+            Text("개별 상세 정보를 열람, 수정할 수 있습니다.")
+                .medium14Black500()
         }
     }
 }
