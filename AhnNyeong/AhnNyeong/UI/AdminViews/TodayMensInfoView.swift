8919153430300a6eb452df75aef81e2c65110aa8
jaelyung <jaekim7724@gmail.com>
2023-11-05 23:34:30 +0900

    [feat] 오늘 생리 중 디테일 정보 컴포넌트로 분리

--- AhnNyeong/AhnNyeong/UI/AdminViews/TodayMensInfoView.swift
+++ AhnNyeong/AhnNyeong/UI/AdminViews/TodayMensInfoView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 struct TodayMensInfoView: View {
     var title: String
-    var mensDataCount = 0
+    var mensDataCount = 10
     let column = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     var body: some View {
         HStack {
-            VStack(alignment: .leading) {
-                Text(title)
-                    .bold24White50()
-                HStack(spacing: 10) {
+            VStack {
+                HStack {
+                    Text(title)
+                        .bold24White50()
+                    Spacer()
+                }
+                HStack {
                     if mensDataCount == 0 {
                         Text("오늘 생리 중인 사람이 없습니다.")
                             .medium16White50()
@@ -25,30 +28,35 @@ struct TodayMensInfoView: View {
                     } else {
                         LazyVGrid(columns: column, spacing: 10, content: {
                             ForEach(0..<mensDataCount) { _ in
-                                HStack {
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        Text("김이최박생리")
-                                            .medium16Black500()
-                                        HStack(spacing: 5) {
-                                            Image("DropFill")
-                                            Text("1일째")
-                                                .medium16Black500()
-                                        }
-                                    }
-                                    Spacer()
-                                }
-                                .padding(10)
-                                .background(TodayMensInfoDetailBox())
-                                .frame(width: 102, height: 63)
+                                TodayMensInfoDetailView()
                             }
                         })
                     }
                 }
             }
-            Spacer()
         }
         .padding(EdgeInsets(top: 20, leading: 16, bottom: 20, trailing: 16))
         .background(TodayMensInfoBackground())
+    }
+}
+
+struct TodayMensInfoDetailView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 5) {
+                Text("김이최박생리")
+                    .medium16Black500()
+                HStack(spacing: 5) {
+                    Image("DropFill")
+                    Text("1일째")
+                        .medium16Black500()
+                }
+            }
+            Spacer()
+        }
+        .padding(10)
+        .background(TodayMensInfoDetailBox())
+        .frame(height: 63)
     }
 }
 
