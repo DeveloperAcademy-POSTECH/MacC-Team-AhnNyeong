979a80e5cd0daab4971ddff8e392679b417529ca
jaelyung <jaekim7724@gmail.com>
2023-11-05 13:31:07 +0900

    [feat] 관리자 메인화면 실행시 생리 시작 지연 알림 기능 추가 (알림 중단 기능 및 추가적인 action 필요)

--- AhnNyeong/AhnNyeong/UI/AdminViews/AdminMainView.swift
+++ AhnNyeong/AhnNyeong/UI/AdminViews/AdminMainView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AdminMainView: View {
     @Binding var selectedUserType: ContentView.LoginType
     @State private var showNew = false
+    @State private var checkMensNotStarted = false
     let dateformat: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "Y년 M월 d일(EEE)"
@@ -29,6 +30,19 @@ struct AdminMainView: View {
 //                        Spacer()
 //                    }
                     TodayMensInfoView(title: "오늘 생리 중")
+                        .onAppear {
+                            // if 생리 시작 입력이 예정일로부터 14일간 입력되지 않은 사람이 있으면
+                            checkMensNotStarted = true
+                        }
+                        .alert(isPresented: $checkMensNotStarted) {
+                            Alert(
+                                title: Text("생리 시작 지연 알림"),
+                                message: Text("하월경의 생리 시작 입력이 예정일로부터 \n14일간 입력되지 않고 있습니다."),
+                                primaryButton: .default(Text("알림 중단"), action: {
+                                    // some Action
+                                }),
+                                secondaryButton: .default(Text("확인")))
+                        }
                     NextMensInfoView(title: "곧 생리 예정")
                     Spacer()
                 }
