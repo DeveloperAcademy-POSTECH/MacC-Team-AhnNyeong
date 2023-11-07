88512149be93a055878969d8b7e617247561f3a0
oceanooooo <funboard6287@gmail.com>
2023-11-08 00:39:06 +0900

    [feat] 날짜 선택 시의 이벤트 추가 표시 - 선택 즉시 반영 - 대상자용 물방울 배치 - 관리자용 물방울(+라벨) 배치

--- AhnNyeong/AhnNyeong/UI/CalendarViewController.swift
+++ AhnNyeong/AhnNyeong/UI/CalendarViewController.swift
@@ -12,15 +12,17 @@ import SnapKit
 import Then
 
 class CalendarViewController: UIViewController, FSCalendarDelegate {
-    let coral500 = UIColor(red: 255/255, green: 84/255, blue: 61/255, alpha: 1)   // coral500
+    let coral500 = UIColor(red: 255/255, green: 84/255, blue: 61/255, alpha: 1) // coral500
     let black500 = UIColor(red: 31/255, green: 31/255, blue: 31/255, alpha: 1) // black500
-    let calmint = UIColor(red: 32/255, green: 211/255, blue: 211/255, alpha: 1)   // cal_weekend
-    let calyellow = UIColor(red: 255/255, green: 236/255, blue: 165/255, alpha: 1) // calToday
+    let black75 = UIColor(red: 171/255, green: 171/255, blue: 171/255, alpha: 1)    // black75
+    let white50 = UIColor(red: 255/255, green: 255/255, blue: 254/255, alpha: 1)    // white50
+    let calmint = UIColor(red: 32/255, green: 211/255, blue: 211/255, alpha: 1) // cal_weekend
+    let calyellow = UIColor(red: 255/255, green: 236/255, blue: 165/255, alpha: 1)  // calToday
 
     var eventsArray = [Date]()  // 이벤트(예정)
     var eventsArrayDone = [Date]()  // 이벤트(완료)
 
-    var isTarget = true
+    var isWeekly = true
 
     // MARK: - Constant
     struct Icon {
@@ -57,6 +59,12 @@ class CalendarViewController: UIViewController, FSCalendarDelegate {
         $0.timeZone = TimeZone(identifier: "KST")
     }
 
+    let dayOnlyFormatter = DateFormatter().then {
+        $0.dateFormat = "d일"
+        $0.locale = Locale(identifier: "ko_kr")
+        $0.timeZone = TimeZone(identifier: "KST")
+    }
+
     // MARK: - UI components
     private lazy var calendarView = FSCalendar(frame: .zero)
 
@@ -70,7 +78,7 @@ class CalendarViewController: UIViewController, FSCalendarDelegate {
     }
 
     private lazy var toggleButton = UIButton().then {
-        $0.setTitle(isTarget == false ? "월" : "주", for: .normal)
+        $0.setTitle(isWeekly == false ? "월" : "주", for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 16.0)
         $0.setTitleColor(.label, for: .normal)
         $0.setImage(Icon.downIcon, for: .normal)
@@ -147,8 +155,9 @@ extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegateAppear
         } else {
             calendarView.appearance.titleSelectionColor = black500
         }
-
 //        viewDidLoad()
+//        configureUI()
+        calendarView.reloadData()
     }
 
     // 주말 색상 변경
@@ -166,8 +175,7 @@ extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegateAppear
 
     // 캘린더 오늘/선택 날짜 표시
     func calendar(_ calendar: FSCalendar, willDisplay cell: FSCalendarCell, for date: Date, at monthPosition: FSCalendarMonthPosition) {
-        let eventScaleFactor: CGFloat = 24/8
-        cell.eventIndicator.transform = CGAffineTransform(scaleX: eventScaleFactor, y: eventScaleFactor*1.5)
+        cell.eventIndicator.transform = CGAffineTransform(scaleX: 0, y: 0)
 
         // 오늘 일자 타이틀 색상 지정  // not completed -> HARD coding
         let day = Calendar.current.component(.weekday, from: date) - 1
@@ -178,22 +186,76 @@ extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegateAppear
         } else {
             calendarView.appearance.titleTodayColor = black500
         }
-    }
 
-    // 캘린더 오늘/선택 날짜 표시 위치
-    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, eventOffsetFor date: Date) -> CGPoint {
-        return CGPoint(x: 0, y: 0)
+        // 칸 배경색
+        let selectedColor = CGColor(red: 255/255, green: 236/255, blue: 165/255, alpha: 1)
+        let todayColor = CGColor(red: 255/255, green: 236/255, blue: 165/255, alpha: 0.5)
+        if date == calendarView.today {
+            cell.layer.backgroundColor = todayColor
+        } else if date == calendarView.selectedDate {
+            cell.layer.backgroundColor = selectedColor
+        } else {
+            cell.layer.backgroundColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0)
+        }
+        cell.layer.borderWidth = 0.5
+        cell.layer.borderColor = CGColor(red: 171/255, green: 171/255, blue: 171/255, alpha: 1)
+
+        // 셀에 image 추가
+        var iconName: String {
+            if self.eventsArray.contains(date) {
+                return "drop"
+            } else if self.eventsArrayDone.contains(date) {
+                return "drop.fill"
+            } else {
+                return ""
+            }
+        }
+        /*
+        // 대상자 물방울
+        let cellImage = UIImage(systemName: iconName)?.resize(newWidth: cell.bounds.width/2).withTintColor(coral500)
+        cell.backgroundView = UIImageView(image: cellImage)
+        cell.backgroundView?.snp.makeConstraints {
+            let imageOffset = 4.0
+            $0.left.equalToSuperview().offset(imageOffset)
+            $0.bottom.equalTo(cell).offset(-3*imageOffset)  // not completed -> HARD coding
+        }
+         */
+
+        // 관리자용 물방울 & 이벤트 수량 라벨 이미지
+        if (self.eventsArray.contains(date) || self.eventsArrayDone.contains(date)) {
+            let cellImageView = UIImageView(image: UIImage(systemName: iconName))
+            cellImageView.tintColor = coral500
+            cellImageView.translatesAutoresizingMaskIntoConstraints = true
+            let cellImageLable = UILabel().then { [weak self] in
+                guard let self = self else { return }
+                $0.font = .systemFont(ofSize: 18, weight: .semibold)
+                $0.textColor = date > Date() ? coral500 : white50
+                $0.text = "\(date > Date() ? self.eventsArray.filter { $0 == date }.count :  self.eventsArrayDone.filter { $0 == date }.count)"
+                }
+            cell.contentView.addSubview(cellImageView)
+            cellImageView.addSubview(cellImageLable)
+            cellImageView.snp.makeConstraints {
+                $0.centerX.equalToSuperview()
+                $0.centerY.equalTo(cell.snp.centerY).offset(12.0) // 숫자 레이블 위치 조정
+                $0.width.equalTo(32)
+                $0.height.equalTo(43)
+            }
+            cellImageLable.snp.makeConstraints {
+                $0.centerX.equalToSuperview()
+                $0.center.equalTo(cellImageView.snp.center).offset(4.0) // 숫자 레이블 위치 조정
+            }
+        } else {
+            for subview in cell.contentView.subviews {
+                if subview is UIImageView {
+                    subview.removeFromSuperview()
+                }
+            }
+        }
     }
 
     // 이벤트 기본 색상
     func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, eventDefaultColorsFor date: Date) -> [UIColor]? {
-        if self.eventsArray.contains(date) {
-            return [UIColor.yellow]
-        }
-        if self.eventsArrayDone.contains(date) {
-            return [UIColor.purple]
-        }
-        return nil
+        return [UIColor.clear]
     }
 
     // 이벤트 표시 갯수
@@ -205,42 +267,44 @@ extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegateAppear
         }
     }
 
-    // DateCell의 이미지
-    func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
-        var iconName: String {
-            if self.eventsArray.contains(date) {
-                return "drop"
-            } else if self.eventsArrayDone.contains(date) {
-                return "drop.fill"
-            } else {
-                return "square"
-            }
-        }
-        return UIImage(systemName: iconName)?.resize(newWidth: 24).withTintColor(coral500)
-    }
+//    // DateCell의 이미지
+//    func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
+//        // 셀에 addView
+//        var iconName: String {
+//            if self.eventsArray.contains(date) {
+//                return "drop"
+//            } else if self.eventsArrayDone.contains(date) {
+//                return "drop.fill"
+//            } else {
+//                return ""
+//            }
+//        }
+//        let cellImage = UIImage(systemName: iconName)?.resize(newWidth: 32).withTintColor(coral500.withAlphaComponent(0.5))
+//        return cellImage
+//    }
 
     // 일자 표시 위치
     func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleOffsetFor date: Date) -> CGPoint {
         let titleOffset = -8.0
-        return CGPoint(x: titleOffset, y: titleOffset*1.5)
+        return CGPoint(x: titleOffset, y: titleOffset*3)
+//        return CGPoint(x: 0, y: 0)
     }
-
-//    func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
-//        var count = 0
-//        if self.eventsArray.contains(date) {
-//            count += 1
-//            return "\(count)"
-//        } else if self.eventsArrayDone.contains(date) {
-//            count += 1
-//            return "\(count)"
-//        } else {
-//            return "square"
-//        }
-//    }
 }
 
 // MARK: - Method
 extension CalendarViewController {
+    private func configureCalendar() {
+        configureCalendarBasic()
+        configureCalendarUI()
+        sampleColoring()
+
+        calendarView.placeholderType = .none
+
+        calendarView.scrollEnabled = true
+        calendarView.scrollDirection = .horizontal
+    }
+
+    // MARK: - Calendar Components UI
     private func configureUI() {
         // 캘린더 버튼 배치
         let calendarButtonStackView = UIStackView(arrangedSubviews: [leftButton, headerLabel, rightButton]).then {
@@ -263,14 +327,14 @@ extension CalendarViewController {
         }
         [calendarView, calendarButtonStackView, changeButtonStackView, testLabel, selectedDateLabel].forEach { view.addSubview($0) }
 
-        // UI 레이아웃
+        // MARK: - UI AutoLayout
         calendarView.snp.makeConstraints {  // not completed -> sample
 //            $0.top.equalTo(view.safeAreaLayoutGuide).offset(32.0)
             $0.top.equalTo(calendarButtonStackView.snp.bottom).offset(8.0)
             $0.trailing.leading.equalToSuperview().inset(12.0)
 //            $0.height.equalTo(view.bounds.height*3/5)    // 달력 높이
-            $0.height.equalTo(calendarView.rowHeight*8)    // 달력 높이
-            print(calendarView.rowHeight)
+            calendarView.rowHeight = 91
+            $0.height.equalTo(calendarView.rowHeight*6 + calendarView.headerHeight)    // 달력 높이
         }
         calendarButtonStackView.snp.makeConstraints {
 //            $0.centerY.equalTo(calendarView.calendarHeaderView.snp.centerY)
@@ -293,47 +357,49 @@ extension CalendarViewController {
         }
     }
 
-    private func configureCalendar() {
-        configureCalendarBasic()
-        configureCalendarUI()
-        sampleColoring()
-
-        calendarView.placeholderType = .none
-
-        calendarView.scrollEnabled = true
-        calendarView.scrollDirection = .horizontal
-    }
-
     private func configureCalendarBasic() {
         calendarView.delegate = self
         calendarView.dataSource = self
         calendarView.select(Date())
         calendarView.locale = Locale(identifier: "ko_KR")
-        calendarView.scope = isTarget == false ? .month : .week // 기본 달력 형태(월간/주간)
+        calendarView.scope = isWeekly == false ? .month : .week // 기본 달력 형태(월간/주간)
+        calendarView.adjustsBoundingRectWhenChangingMonths = true
     }
 
+    // MARK: - Calendar UI
     private func configureCalendarUI() {
         // header: yyyy년 mm월, weekday: 요일, title: 일자
         calendarView.appearance.headerTitleColor = .clear
         calendarView.appearance.headerMinimumDissolvedAlpha = 0.0
-        calendarView.appearance.selectionColor = calyellow
-        calendarView.appearance.borderRadius = 0.0
+        calendarView.appearance.selectionColor = .clear
+        calendarView.appearance.todayColor = .clear
+        calendarView.appearance.separators = FSCalendarSeparators(rawValue: 1)!
         calendarView.weekdayHeight = 24.0
-        calendarView.rowHeight = 64.0
         calendarView.headerHeight = 0.0
         calendarView.appearance.weekdayFont = .systemFont(ofSize: 14.0, weight: .bold)
         calendarView.appearance.titleFont = .systemFont(ofSize: 14.0, weight: .bold)
-        calendarView.appearance.titleDefaultColor = black500
-        calendarView.appearance.todayColor = calyellow.withAlphaComponent(0.5)
         calendarView.appearance.weekdayTextColor = black500
+        calendarView.appearance.titleDefaultColor = black500
+        calendarView.collectionViewLayout.sectionInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)    // cell 여백 조정
+        calendarView.appearance.imageOffset = CGPoint(x: 0, y: -10)
+
+        let borderColor = CGColor(red: 171/255, green: 171/255, blue: 171/255, alpha: 1)
+//        calendarView.calendarWeekdayView.layer.borderWidth = 0.5
+//        calendarView.calendarWeekdayView.layer.borderColor = borderColor
+        calendarView.daysContainer.layer.borderWidth = 0.5
+        calendarView.daysContainer.layer.borderColor = borderColor
+//        calendarView.layer.addBorder([.bottom, .left, .right], color: black500, width: 0.5)
+        calendarView.layer.borderWidth = 0.5
+        calendarView.layer.borderColor = borderColor
     }
 
     private func sampleColoring() { // sample
-        calendarView.backgroundColor = .lightGray.withAlphaComponent(0.2)
-        calendarView.calendarWeekdayView.backgroundColor = .orange.withAlphaComponent(0.2)
-        calendarView.daysContainer.backgroundColor = .purple.withAlphaComponent(0.2)
+//        calendarView.backgroundColor = .lightGray.withAlphaComponent(0.2)
+//        calendarView.calendarWeekdayView.backgroundColor = .orange.withAlphaComponent(0.2)
+//        calendarView.daysContainer.backgroundColor = .purple.withAlphaComponent(0.2)
     }
 
+    // MARK: - goto Prev/Next
     func getNextMonth(date: Date) -> Date {
         return  Calendar.current.date(byAdding: .month, value: 1, to: date)!
     }
@@ -357,19 +423,17 @@ extension CalendarViewController {
 
     @objc func tapToggleButton() {
         if self.calendarView.scope == .month {
-            self.calendarView.setScope(.week, animated: true)
+            self.calendarView.setScope(.week, animated: false)
             self.headerDateFormatter.dateFormat = "YYYY년 MM월 W주차"
+            self.headerLabel.text = headerDateFormatter.string(from: calendarView.currentPage)
             self.toggleButton.setTitle("주", for: .normal)
             self.toggleButton.setImage(Icon.downIcon, for: .normal)
-            self.headerLabel.text = headerDateFormatter.string(from: calendarView.currentPage)
-            self.calendarView.rowHeight = 90
         } else {
-            self.calendarView.setScope(.month, animated: true)
+            self.calendarView.setScope(.month, animated: false)
             self.headerDateFormatter.dateFormat = "YYYY년 MM월"
+            self.headerLabel.text = headerDateFormatter.string(from: calendarView.currentPage)
             self.toggleButton.setTitle("월", for: .normal)
             self.toggleButton.setImage(Icon.upIcon, for: .normal)
-            self.headerLabel.text = headerDateFormatter.string(from: calendarView.currentPage)
-            self.calendarView.rowHeight = 64
         }
     }
 
@@ -402,6 +466,33 @@ extension UIImage {
             self.draw(in: CGRect(origin: .zero, size: size))
         }
         return renderImage
+    }
+}
+
+extension CALayer {
+    func addBorder(_ arrEdge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arrEdge {
+            let border = CALayer()
+            switch edge {
+                case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor
+            self.addSublayer(border)
+        }
     }
 }
 
